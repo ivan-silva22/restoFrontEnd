6ddb1f5b06b600824c41746572d0acfddd781bee
Ivan Silva <silvaivan280@gmail.com>
2024-05-10 14:42:58 -0300

    agregue el maquetado del componente Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/common/Menu';
 
 function App() {
  
 
   return (
     <>
-      
+      <Menu/>
     </>
   )
 }
---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,26 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../../assets/logo.svg"
+
+const Menu = () => {
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img className="logo" src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Pedidos</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+            <Nav.Link href="#link">Iniciar sesión</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
